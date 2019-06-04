7b4061a5491ba5a0d4d7dc832738b3f27b6afb7d
Neil McFadden <nmcfadde@unm.edu>
2019-06-04 13:22:10 -0600

    committing most up to date anaOptical for constructing LGND-200 14 string map

--- anaOptical.cc
+++ anaOptical.cc
@@ -51,17 +51,19 @@ int main(){
 
   std::map<std::vector<Int_t>,Double_t> prob_map;
   //Double_t gridSpacing = 2;//*mm
+  Double_t QE = 1.;//0.164;
   Double_t gridSpacing = 5;//*mm
   //Double_t maxX = 320.,maxY=320.0,maxZ=600,minZ = -200.,minR = 0,maxR=320;
   //Double_t maxX = 1000.,maxY=1000,maxZ=1200,minZ = -800.,minR = 320., maxR = 1000.;
   //Double_t maxX = 300.,minX = 0,maxY=300.,minY = 0,maxZ=925,minZ = -425.,minR = 0., maxR = 300.;
-  Double_t maxX = 300.,minX = -300.,maxY=300.,minY = -300,maxZ=860,minZ = -860,minR = 0, maxR = 300.;
-  Int_t nbinsX = (maxX-minX)/gridSpacing,nbinsY = (maxY-minY)/gridSpacing,nbinsZ = (maxZ-minZ)/gridSpacing;
+  Double_t maxX = 300.,minX = -300.,maxY=300.,minY = -300,maxZ= 850,minZ = -850,minR = 0, maxR = 300.;
+  //Double_t maxX = 700.,minX = -700.,maxY=700.,minY = -700,maxZ= 850,minZ = -850,minR = 300, maxR = 700.;
+  Int_t nbinsX = (maxX-minX)/gridSpacing,nbinsY = (maxY-minY)/gridSpacing,nbinsZ = (maxZ-minZ)/gridSpacing, nbinsR = (maxR-minR)/gridSpacing;
   TH3D* hMap = new TH3D("OpticalMap","OpticalMap",nbinsX,minX,maxX,nbinsY,minY,maxY,nbinsZ,minZ,maxZ);
   TH3D* hMapUnscaled = new TH3D("OpticalMap_unScaled","OpticalMap_unScaled",nbinsX,minX,maxX,nbinsY,minY,maxY,nbinsZ,minZ,maxZ);
 
-  TH2D* h2DMapRZ = new TH2D("2DOpticalMap_RZ","2DOpticalMap_RZ",nbinsX/2.,minR,maxR,nbinsZ,minZ,maxZ);
-  TH2D* h2DMapRZUnscaled = new TH2D("2DOpticalMap_RZUnscaled","2DOpticalMap_RZUnscaled",nbinsX/2.,minR,maxR,nbinsZ,minZ,maxZ);
+  TH2D* h2DMapRZ = new TH2D("2DOpticalMap_RZ","2DOpticalMap_RZ",nbinsR,minR,maxR,nbinsZ,minZ,maxZ);
+  TH2D* h2DMapRZUnscaled = new TH2D("2DOpticalMap_RZUnscaled","2DOpticalMap_RZUnscaled",nbinsR,minR,maxR,nbinsZ,minZ,maxZ);
 
   TH2D* h2DMapXY = new TH2D("2DOpticalMap_XY","2DOpticalMap_XY",nbinsX,minX,maxX,nbinsY,minY,maxY);
   TH2D* h2DMapXYUnscaled = new TH2D("2DOpticalMap_XYUnscaled","2DOpticalMap_XYUnscaled",nbinsX,minX,maxX,nbinsY,minY,maxY);
@@ -74,36 +76,13 @@ int main(){
 
   cout<<"starting run"<<endl; 
   TFile * infile;
-  for(int k =0; k < 1 ; k++){
+  for(int k =0; k <= 339; k++){
     //TODO--change input dir
-    //TString dir = "/mnt/mjdDisk1/Majorana/users/nmcfadden/MaGe/bin/Linux-g++/";
-    //TString dir = "/mnt/mjdDisk1/Majorana/users/nmcfadden/BACoN/bin/Linux-g++/";
-    //TString dir = "/mnt/mjdDisk1/Majorana/users/nmcfadden//LEGEND_200_One_FIBER_Array/";
-    //TString dir = "/mnt/mjdDisk1/Majorana/users/nmcfadden/LGND_200Orig100M1.1mAttenuation/";
-    //TString dir = "/mnt/mjdDisk1/Majorana/users/nmcfadden/RooT/";
-    //TString dir = "/mnt/mjdDisk1/Majorana/users/nmcfadden/";
-    //TString dir = "/mnt/mjdDisk1/Majorana/users/nmcfadden/array/";
-    //TString dir = "/mnt/mjdDisk1/Majorana/users/nmcfadden/arrOpticalDist/";
-    //TString dir = "/mnt/mjdDisk1/Majorana/users/nmcfadden/LGND19stringNoHV/";
-    //TString dir = "/mnt/mjdDisk1/Majorana/users/nmcfadden/ExteriorMap/";
-    //TString dir = "/mnt/mjdDisk1/Majorana/users/nmcfadden/opticalArray/";
-    //TString dir = "/mnt/mjdDisk1/Majorana/users/nmcfadden/19StringRadon700/";
-    //TString dir = "/mnt/mjdDisk1/Majorana/users/nmcfadden/14StringRadon700/";
-    TString dir = "/home/nmcfadden/MaGe/bin/Linux-g++/";
-    //TString fileName = "SensitiveVolumesLGND_200Alt1" +to_string(k);
-    //TString fileName = "SensitiveVolumesLGND_200Orig1.1mAttenuation" +to_string(k);
-    //TString fileName = "SensitiveVolumesLGND_200Orig" +to_string(k);
-    //TString fileName = "SensitiveVolumes" +to_string(k);
-    //TString fileName = "SensitiveVolumesLGND_200Orig"+to_string(k);
-    //TString fileName = "RDMiso224.88.Optical";
-    //TODO -- change fileName
-    //TString fileName = "ExteriorMap_"+to_string(k);
-    //TString fileName = "OpticalRunExterior"+to_string(k);
-    //TString fileName = "OpticalRun14String"+to_string(k); 
-    //TString fileName = "OpticalRun19StringExterior"+to_string(k); 
-    //TString fileName = "OpticalRun14StringExterior"+to_string(k); 
-    //TString fileName = "OpticalRun"+to_string(k);
-    TString fileName = "SpeedTest";
+    TString dir = "";
+    dir = "/data4/legend/sim/l200sims/LGND200_14String_Optical/default/";
+    TString fileName = "";
+    fileName = "14StringBaseline"+to_string(k);
+    //TString fileName = "14StringBaselineExterior"+to_string(k);
     cout<<"File location at: "<<dir+fileName+TString(".root")<<endl;;
     if(!fileExist(string(dir+fileName+TString(".root")))){
       cout<<"processed "<<k<<" files"<<endl;
@@ -118,41 +97,51 @@ int main(){
     else
       infile = TFile::Open(dir+fileName+TString(".root"));
     if(infile->Get("NumberOfEvents") == NULL) {
-      cout<<"file has not closed yet"<<endl;
+      cout<<"NumberOfEvents is NULL... maybe file has not closed yet"<<endl;
       continue;
     }
     string neventsString = infile->Get("NumberOfEvents")->GetTitle();
     totalEvents += std::stoi(neventsString,nullptr,10);
 
     string nprimariesString = infile->Get("NumberOfPrimaries")->GetTitle();
-    totalPrimaries += std::stoi(nprimariesString,nullptr,10);
-
+    if(fileName.Contains("Exterior")) totalPrimaries += 78400;//std::stoi(nprimariesString,nullptr,10);
+    else totalPrimaries += std::stoi(nprimariesString,nullptr,10);
+    cout<<"adding to total Primaries "<<std::stoi(nprimariesString,nullptr,10)<<"... Total Primaries is "<<totalPrimaries<<endl;
     infile->Close();
     delete infile;
     outFileMaGe->cd();
     
     TChain *fTree = new TChain("fTree");
     fTree->Add(dir+fileName+TString(".root"));
-    
+    if(fTree == NULL) break; 
     Long64_t nentries = (Long64_t)fTree->GetEntries();
     
     cout<<". Processing "<<nentries<<" entries"<<endl;
     MGTMCEventSteps *eventSteps = 0;
     MGTMCEventSteps *eventPrimaries = 0;
-    if(fTree != NULL){
+    
+    if(fTree->GetBranchStatus("eventSteps")){
       fTree->SetBranchAddress("eventSteps",&eventSteps);
+    }
+    else{
+      cout<<"eventSteps is NULL"<<endl;
+      continue;
+    }
+
+    if(fTree->GetBranchStatus("eventPrimaries")){
       fTree->SetBranchAddress("eventPrimaries",&eventPrimaries);
     }
     else{
-      cout<<"NULL fTree"<<endl;
-      break;
-    }
-       
+      cout<<"eventSteps is NULL"<<endl;
+      continue;
+    }  
+    
     const MGTMCStepData *step,*primaries;
     
     for(Int_t i = 0; i < nentries ; i++){
       if((i+1)%10000 == 0 || i == nentries - 1 ) cout<<"\tprocessed "<<i+1<<" events"<<endl;
-      fTree->GetEntry(i);
+      //if(i==0) break;
+	    fTree->GetEntry(i);
       TString physName;
       primaries = eventPrimaries->GetStep(0);
       if(primaries == NULL){
@@ -182,7 +171,7 @@ int main(){
         //Track weight == 1 is for scintillation, track weight == 2 is for OpWLS
         if(physName.Contains("SiPM") && step->GetEdep() > 0 && step->GetTrackWeight() == 2 && pastTrackID != trackID){
           ntpleStepSiPM->Fill(i,step->GetEdep(),step->GetKineticE(),step->GetX(),step->GetY(),step->GetZ(),sqrt(step->GetX()*step->GetX() +step->GetY()*step->GetY()),x,y,z,r,theta);
-          //cout<<"\tx "<<x<<", y "<<y<<" z "<<z<<" volName "<< physName<<endl;
+          //cout<<"\tx "<<x<<", y "<<y<<" z "<<z<<" r "<<r<<" volName "<< physName<<endl;
           hMapUnscaled->Fill(x,y,z);
           h2DMapRZUnscaled->Fill(r,z);
           h2DMapXYUnscaled->Fill(x,y);
@@ -197,11 +186,11 @@ int main(){
     delete fTree;
   }
   //weight 3Dhistogram
-  cout<<hMap->GetNbinsX()<<" "<<hMap->GetNbinsY()<<" "<<hMap->GetNbinsZ()<<endl;
+  cout<<hMap->GetNbinsX()<<" "<<hMap->GetNbinsY()<<" "<<hMap->GetNbinsZ()<<" rBins "<<h2DMapRZUnscaled->GetNbinsX()<<endl;
   for(int i = 0; i < hMap->GetNbinsX();i++){
     for(int j = 0; j < hMap->GetNbinsY();j++){
       for(int k = 0; k < hMap->GetNbinsZ();k++){
-        Double_t binVal = hMapUnscaled->GetBinContent(i,j,k);
+        Double_t binVal = hMapUnscaled->GetBinContent(i+1,j+1,k+1)*QE;
         Double_t x = gridSpacing*(i)-maxX;
         Double_t y = gridSpacing*(j)-maxY;
         Double_t z = gridSpacing*(k)+minZ;
@@ -210,47 +199,53 @@ int main(){
             //cout<<"("<<x<<","<<y<<","<<z<<") ... bin center value"<<endl;
             n3D++;
         }
-        hMap->SetBinContent(i,j,k,binVal/(totalPrimaries/(nbinsX*nbinsY*nbinsZ)));//totalEvents);
+        //hMap->SetBinContent(i+1,j+1,k+1,binVal/(totalPrimaries/(nbinsX*nbinsY*nbinsZ)));//totalEvents);
+        hMap->SetBinContent(i+1,j+1,k+1,binVal/(500));//totalPrimaries/(nbinsX*nbinsY*nbinsZ)));//totalEvents);
       }
     }
   }
   cout<<"finished XYZ Histogram"<<endl;
   for(int i = 0; i < h2DMapXY->GetNbinsX();i++){
     for(int j = 0; j < h2DMapXY->GetNbinsY();j++){
-      Double_t binVal = h2DMapXYUnscaled->GetBinContent(i,j);
+      Double_t binVal = h2DMapXYUnscaled->GetBinContent(i+1,j+1)*QE;
       Double_t x = gridSpacing*(i)-maxX;
       Double_t y = gridSpacing*(j)-maxY;
       if(binVal == 0){
          //binVal = h2DMapXYUnscaled->Interpolate(x,y);
          nXY++;
       }
-      h2DMapXY->SetBinContent(i,j,binVal/(totalPrimaries/(nbinsX*nbinsY)));//totalEvents);
+      h2DMapXY->SetBinContent(i+1,j+1,binVal/(totalPrimaries/(nbinsX*nbinsY)));//totalEvents);
     }
   }
   cout<<"finished XY Histogram"<<endl;
   for(int i = 0; i < h2DMapYZ->GetNbinsX();i++){
     for(int j = 0; j < h2DMapYZ->GetNbinsY();j++){
-      Double_t binVal = h2DMapYZUnscaled->GetBinContent(i,j);
-      Double_t y = gridSpacing*(i)-maxY;
-      Double_t z = gridSpacing*(j)+minZ;
+      Double_t binVal = h2DMapYZUnscaled->GetBinContent(i+1,j+1)*QE;
+      Double_t y = gridSpacing*(i+1)-maxY;
+      Double_t z = gridSpacing*(j+1)+minZ;
       if(binVal == 0){
         //binVal = h2DMapYZUnscaled->Interpolate(y,z);
         nYZ++;
       }
-      h2DMapYZ->SetBinContent(i,j,binVal/(totalPrimaries/(nbinsX*nbinsY)));//totalEvents);
+      h2DMapYZ->SetBinContent(i+1,j+1,binVal/(totalPrimaries/(nbinsX*nbinsY)));//totalEvents);
     }
   }
   cout<<"finished YZ Histogram"<<endl;
+TH1D * hWeight = new TH1D("rWeight","rWeight",h2DMapRZ->GetNbinsX(),minR,maxR);
   for(int i = 0; i < h2DMapRZ->GetNbinsX();i++){
     for(int j = 0; j < h2DMapRZ->GetNbinsY();j++){
-      Double_t binVal = h2DMapRZUnscaled->GetBinContent(i,j);
-      Double_t r = gridSpacing*(i)+minR;
-      Double_t z = gridSpacing*(j)+minZ;
+      Double_t binVal = h2DMapRZUnscaled->GetBinContent(i+1,j+1)*QE;
+      Double_t r = gridSpacing*(i+1)+minR;
+      Double_t z = gridSpacing*(j+1)+minZ;
+      Double_t weight = ((r+gridSpacing)*(r+gridSpacing)-r*r)/(maxR*maxR-minR*minR);
+      binVal /= weight;
       if(binVal == 0){
         //binVal = h2DMapRZUnscaled->Interpolate(r,z);
         nRZ++; 
       }
-    h2DMapRZ->SetBinContent(i,j,binVal/(totalPrimaries/(nbinsX*nbinsZ/2.)));//totalEvents);
+      hWeight->SetBinContent(i+1,weight);
+    //h2DMapRZ->SetBinContent(i+1,j+1,binVal/(totalPrimaries/(nbinsR*nbinsZ)));//totalEvents);
+    h2DMapRZ->SetBinContent(i+1,j+1,binVal/(totalPrimaries/(nbinsZ)));//totalEvents);
     }
   }
   cout<<"finished RZ Histogram"<<endl;
@@ -259,8 +254,8 @@ int main(){
   outFileMaGe->Close();
   cout<<"Total events = "<<totalEvents<<", total primaries "<<totalPrimaries<<endl;
   cout<<"Events per bin..."<<totalPrimaries/(nbinsX*nbinsY)<<" XY, "<<totalPrimaries/(nbinsY*nbinsZ)
-    <<" YZ, "<<totalPrimaries/(nbinsX*nbinsZ/2.)<<" RZ, "<<totalPrimaries/(nbinsX*nbinsY*nbinsZ)<<" XYZ"<<endl;
-  cout<<"Histogram has "<<nbinsX*nbinsY<<" XY bins, "<<nbinsY*nbinsZ<<" YZ bins, "<<nbinsZ*nbinsX/2<<" RZ bins, "<<nbinsX*nbinsY*nbinsZ <<", xyz bins"<<endl;
+    <<" YZ, "<<totalPrimaries/(nbinsR*nbinsZ)<<" RZ, "<<totalPrimaries/(nbinsX*nbinsY*nbinsZ)<<" XYZ"<<endl;
+  cout<<"Histogram has "<<nbinsX*nbinsY<<" XY bins, "<<nbinsY*nbinsZ<<" YZ bins, "<<nbinsZ*nbinsR<<" RZ bins, "<<nbinsX*nbinsY*nbinsZ <<", xyz bins"<<endl;
   cout<<"Zero bins found..."<<nXY<<" XY, "<<nYZ<<" YZ, "<<nRZ<<" RZ bins, "<<n3D <<", xyz bins"<<endl;
   cout<<"root -l "<<fileName<<endl;
   return 0;
